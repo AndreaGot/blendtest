fc07e199236b00096244e542793d0f372692977a
Andrea Gottardi <andreagot16@gmail.com>
2013-08-26 22:58:31 +0200

    First commit - Basic app

---
+++ BlendTest/BlendTest/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using BlendTest.Resources;
+
+namespace BlendTest
+{
+    /// <summary>
+    /// Fornisce l'accesso alle risorse stringa.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
